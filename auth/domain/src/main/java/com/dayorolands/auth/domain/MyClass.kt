b64e9b3bc693b0b9d78e2ee43f7bb49d7e347493
dayorolands <iadekoya@appzonegroup.com>
2024-06-02 02:25:52 +0100

    Creating convention plugins for Runique project

--- auth/domain/src/main/java/com/dayorolands/auth/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.dayorolands.auth.domain
-
-class MyClass {
-}
--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -35,9 +35,12 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.material3)
+
+    implementation(projects.core.domain)
+    implementation(projects.core.presentation.designsystems)
 }
